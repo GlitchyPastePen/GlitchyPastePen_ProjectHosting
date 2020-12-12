93972cc80c815e95fcabb1a68b34bb95445b8928
Khaleel Gibran <khalby786@gmail.com>
2020-12-12 18:54:44 +0400

    leaked my password, so i changed my password

--- functions/code.js
+++ functions/code.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Project = require("../models/project.js");
 
-mongoose.connect('mongodb+srv://khalby786:autobots786@gppprojectinfo.vzyse.mongodb.net/projects?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
